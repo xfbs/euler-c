1ce5ea004ceca34996024599ee6e8e49ca003c51
Patrick Elsen <pelsen@xfbs.net>
2019-08-23 02:04:30 +0200

    adds solve016

--- src/solve/solve016.c
+++ src/solve/solve016.c
@@ -1,7 +1,7 @@
-#include "solve.h"
-#include "euler/vec8.h"
+#include "solve016.h"
+#include <euler/vec8.h>
 
-uint64_t solve(size_t exp) {
+uint64_t power_digit_sum(size_t exp) {
   vec8_t num = vec8_new(1, 1);
 
   // compute 2^exp
@@ -28,3 +28,22 @@ uint64_t solve(size_t exp) {
 
   return sum;
 }
+
+struct euler_result solve016(json_t *input) {
+  struct euler_result result;
+  int64_t exponent;
+
+  result = euler_input_get(input016, input, &exponent);
+  if(!result.ok) {
+    return result;
+  }
+
+  euler_write(&result, "%llu", power_digit_sum(exponent));
+
+  return result;
+}
+
+const struct euler_input input016[] = {
+  euler_input_number(exponent, "exponent of 2 for which to sum digits", 1000, 0, INT64_MAX),
+  {NULL},
+};
--- src/solve/solve016.h
+++ src/solve/solve016.h
@@ -1,4 +1,7 @@
-#include <euler/common.h>
 #pragma once
+#include "euler.h"
 
-uint64_t solve(size_t exp);
+uint64_t power_digit_sum(size_t exp);
+
+struct euler_result solve016(json_t *input);
+extern const struct euler_input input016[];
