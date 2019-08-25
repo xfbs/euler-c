295716c782cdd90c13873c97732613c38faa13b9
Patrick Elsen <pelsen@xfbs.net>
2019-08-25 20:19:29 +0200

    adds solve025

--- src/solve/solve025.c
+++ src/solve/solve025.c
@@ -1,10 +1,29 @@
-#include "solve.h"
+#include "solve025.h"
 #include <math.h>
 
-uint32_t solve(uint32_t max) {
+uint32_t n_digit_fibonacci_number(uint32_t max) {
   double phi = (1.0 + sqrt(5)) / 2;
 
   double sum1 = 0.5 * log(5);
   double sum2 = (max - 1) * log(10);
   return ceil((sum1 + sum2) / log(phi));
 }
+
+struct euler_result solve025(json_t *input) {
+  struct euler_result result;
+  int64_t digits;
+
+  result = euler_input_get(input025, input, &digits);
+  if(!result.ok) {
+    return result;
+  }
+
+  euler_write(&result, "%llu", n_digit_fibonacci_number(digits));
+
+  return result;
+}
+
+const struct euler_input input025[] = {
+  euler_input_number(max, "maximum number to check.", 1000, 0, INT64_MAX),
+  {NULL},
+};
--- src/solve/solve025.h
+++ src/solve/solve025.h
@@ -1,4 +1,7 @@
-#include <euler/common.h>
 #pragma once
+#include "euler.h"
 
-uint32_t solve(uint32_t max);
+uint32_t n_digit_fibonacci_number(uint32_t max);
+
+struct euler_result solve025(json_t *input);
+extern const struct euler_input input025[];
